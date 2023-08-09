f29e9ffb40f58c09619d9877702a30174e0c59ec
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-08-09 14:48:53 +0630

    update

--- src/main/java/com/starless/http/ResponseMapper.java
+++ src/main/java/com/starless/http/ResponseMapper.java
@@ -3,6 +3,10 @@ package com.starless.http;
 import jakarta.ws.rs.core.Response;
 import java.util.Map;
 
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+
 public class ResponseMapper {
 
     public static Response map(HttpResponse<?> httpResponse) {
@@ -19,4 +23,21 @@ public class ResponseMapper {
 
         return responseBuilder.build();
     }
+
+    public HttpResponseMessage map(HttpResponse<?> customResponse,
+            HttpRequestMessage<?> azureRequest) {
+        HttpResponseMessage.Builder responseBuilder = azureRequest
+                .createResponseBuilder(HttpStatus.valueOf(customResponse.getStatus()));
+
+        // Set headers
+        for (Map.Entry<String, String> header : customResponse.getHeaders().entrySet()) {
+            responseBuilder.header(header.getKey(), header.getValue());
+        }
+
+        // Set body
+        responseBuilder.body(customResponse.getBody());
+
+        return responseBuilder.build();
+    }
+
 }
