93bc980929a81c2d0edbb081203b85dddd0c0fcf
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-08-09 14:32:24 +0630

    updat

--- src/main/java/com/starless/http/App.java
+++ src/main/java/com/starless/http/App.java
@@ -4,10 +4,8 @@ package com.starless.http;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
